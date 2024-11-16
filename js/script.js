d9e218f85f7b4e7ca6b17271afeb8749558cb581
ShyletDee <dlaminishylet8@gmail.com>
2024-11-15 16:02:39 -0800

    update everything

---
+++ js/script.js
@@ -0,0 +1,32 @@
+//Arrays of projects
+const projects = [
+    {
+        title: 'Shoptacle',
+        description: "A shopping website"
+
+    },
+    {
+        title: 'Project 3',
+        description: "A shopping website"
+
+    },
+    {
+        title: 'Project 5',
+        description: "A shopping website"
+
+    }
+];
+//function to load projects
+function loadprojects() {
+    constprojectslist = document.createElementByld('project-list');
+
+    projects.forEach(() => {
+        const projectCard = document.createElement('div');
+        projectCard.className = 
+
+    })
+
+
+
+}
+
